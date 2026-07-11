e610820aaed00cdc7107e7686b54d7c4a0d2a6c9
amir ukbazgi <amirukbazgi@gmail.com>
2026-07-11 12:54:01 +0200

    file been added

---
+++ blog-app/app/page.js
@@ -0,0 +1,13 @@
+"use cllient";
+
+import BlogItem from "@/components/BlogItem";
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <BlogItem />
+    </>
+  );
+}
