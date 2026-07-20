907bd50073eb549a637c7e8e4e0bfef2c77fc752
amir ukbazgi <amirukbazgi@gmail.com>
2026-07-20 21:30:38 +0200

    added files

--- blog-app/app/blogs/[id]/page.jsx
+++ blog-app/app/blogs/[id]/page.jsx
@@ -37,12 +37,76 @@ export default function Page({ params }) {
           </button>
         </div>
         <div className="text-center my-24">
-          <h1>{data.title}</h1>
+          <h1 className="text-2xl sm:text-5xl font-semibold max-w-175 mx-auto">
+            {data.title}
+          </h1>
           <div>
-            <Image src={data.author_img} alt="photo" width={50} height={50} />
-            <p>{data.author}</p>
+            <Image
+              src={data.author_img}
+              alt="photo"
+              width={60}
+              height={60}
+              className="mx-auto mt-6 border border-white rounded-full"
+            />
+            <p className="mt-1 pb-2 text-lg max-w-185 mx-auto">{data.author}</p>
           </div>
         </div>
+      </div>
+      <div className="mx-5 max-w-200 md:mx-auto -mt-25 mb-10">
+        <Image
+          className="border-4 border-white"
+          src={data.image}
+          width={1280}
+          height={720}
+          alt=""
+        />
+        <h1 className="my-8 text-[26px] font-semibold">Introduction:</h1>
+        <p>{data.description}</p>
+        <h3 className="my-5 text-[18px] font-semibold">
+          Step 1: Self-Reflection and Goal
+        </h3>
+        <p className="my-3">
+          Before you can manage your lifestyle, you must have a clear, Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Eaque porro
+          explicabo sed quam quidem libero.
+        </p>
+        <p className="my-3">
+          Before you can manage your lifestyle, you must have a clear, Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Eaque porro
+          explicabo sed quam quidem libero.
+        </p>
+        <h3 className="my-5 text-[18px] font-semibold">
+          Step 2: Self-Reflection and Goal
+        </h3>
+        <p className="my-3">
+          Before you can manage your lifestyle, you must have a clear, Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Eaque porro
+          explicabo sed quam quidem libero.
+        </p>
+        <p className="my-3">
+          Before you can manage your lifestyle, you must have a clear, Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Eaque porro
+          explicabo sed quam quidem libero.
+        </p>
+        <h3 className="my-5 text-[18px] font-semibold">
+          Step 3: Self-Reflection and Goal
+        </h3>
+        <p className="my-3">
+          Before you can manage your lifestyle, you must have a clear, Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Eaque porro
+          explicabo sed quam quidem libero.
+        </p>
+        <p className="my-3">
+          Before you can manage your lifestyle, you must have a clear, Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Eaque porro
+          explicabo sed quam quidem libero.
+        </p>
+        <h3 className="my-5 text-[18px] font-semibold">Conclusion:</h3>
+        <p className="my-3">
+          Managing your lifestyle is a journey that requires commitment, Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Eaque porro
+          explicabo sed quam quidem libero.
+        </p>
       </div>
     </>
   ) : (
